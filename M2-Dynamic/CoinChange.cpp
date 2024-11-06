d332462dcf78b7166a10c666e2c877e761d5bb39
AlinaGeorge <alinagrg03@gmail.com>
2024-11-06 19:10:11 +0530

    Update CoinChange.cpp

--- M2-Dynamic/CoinChange.cpp
+++ M2-Dynamic/CoinChange.cpp
@@ -1,52 +1,63 @@
 #include<iostream>
 #include<limits>
 using namespace std;
-
-int Min(int a,int b){
-	if(a<b){
-		 return a;
+int min(int p,int q){
+	if(p<q){
+		return p;
 	}
 	else{
-		return b;
+		return q;
 	}
 }
 void display(int Arr[],int n){
-	for(int i=0;i<n;i++){
-		cout<<Arr[i]<<" ";
-	}
-	cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<Arr[i]<<" ";
+    }
+    cout<<endl;
 }
-
-int CoinChange(int n,int D[],int m){
-	int F[n+1];
+int coinchange(int D[],int amount,int m){
+	int F[amount+1];
 	F[0]=0;
-	for(int i=1;i<=n;i++){
-		int temp=numeric_limits<int>::max();
+	for(int i=1;i<=amount;i++){
+		int temp=numeric_limits<int>::max()-1;
 		int j=0;
-		while(j<=m and i>=D[j]){
-			temp=Min(F[i-D[j]],temp);
+		while(j<m and i>=D[j]){
+			temp=min(F[i-D[j]],temp);
 			j++;
 		}
 		F[i]=temp+1;
 	}
-	display(F,n);
-	return F[n];
+	cout<<"Function Array:";
+	display(F,amount);
+	int bal=amount;
+	int cc=F[amount];
+	int B[cc];
+	int ind=0;
+	while(bal>0){
+		for(int j=0;j<m;j++){
+			if(bal>=D[j] && F[bal]==F[bal-D[j]]+1){
+				B[ind++]=D[j];
+				bal-=D[j];
+			}
+		}	
+	}
+	cout<<"Path:";
+	display(B,cc);
+	return F[amount];
 }
 int main(){
-	cout<<"Enter the Coin value for which change is generated:";
-	int n;
-	cin>>n;
+	int den;
 	cout<<"Enter the number of denominations:";
-	int m;
-	cin>>m;
-	int D[m];
-	cout<<"Enter the denominations:"<<endl;
-	for(int i=0;i<m;i++){
+	cin>>den;
+	int D[den];
+	cout<<"Enter the denominations:";
+	for(int i=0;i<den;i++){
 		cin>>D[i];
 	}
-	CoinChange(n,D,m);
+	int sum;
+	cout<<"Enter the sum:";
+	cin>>sum;
+	int coins=coinchange(D,sum,den);
+	cout<<"Number of coins:"<<coins<<endl;
 	return 0;
-}
-	
-	
-	
+} 
